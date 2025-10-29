12d0a6288cab31d46d71087bf756e7e99c7f76a4
Invitation-inv <muniranooo@gmail.com>
2025-10-29 12:39:11 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(cors()); // يسمح لأي أصل - مناسب للبدء. للتشدي
 app.use(express.json({ limit: "1mb" }));
 
 // احفظ رابط Google Apps Script في متغيّر بيئي GOOGLE_SCRIPT_URL
-const GA_SCRIPT_URL = process.env.GOOGLE_SCRIPT_URL || "https://script.google.com/macros/s/AK.../exec";
+const GA_SCRIPT_URL = process.env.GOOGLE_SCRIPT_URL || "https://script.google.com/macros/s/AKfycbwVbM7VjX5amZpBTAf7fQKuzyQBuEI6OdRhdODaaBuoq-f5lgjJTE66XElUgS0PMh0/exec";
 
 // endpoint للـ form يرسل البيانات إلى Google Script
 app.post("/submit", async (req, res) => {
@@ -41,3 +41,4 @@ app.get("/", (req, res) => res.send("GA Proxy is running"));
 // start
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port ${port}`));
+
